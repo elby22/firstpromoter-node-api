64de194148a8b99dfe0a67c9a3a3897a89408dc6
Elbert Basolis <elbertbasolis@Elberts-MacBook-Pro.local>
2019-06-22 17:25:54 -0400

    Adds missing lead API export

--- lib/FirstPromoter.d.ts
+++ lib/FirstPromoter.d.ts
@@ -1,5 +1,6 @@
 import * as api from './api';
 export default class Client {
     promoter: api.PromoterApi;
+    lead: api.LeadApi;
     constructor(apiKey: string);
 }
--- src/FirstPromoter.ts
+++ src/FirstPromoter.ts
@@ -1,9 +1,11 @@
 import * as api from './api';
 export default class Client {
     public promoter: api.PromoterApi;
-
+    public lead: api.LeadApi;
     constructor(apiKey: string){
         if(!apiKey) throw new Error('Must provide a valid API key');
+
         this.promoter = new api.PromoterApi(apiKey);
+        this.lead = new api.LeadApi(apiKey);
     }
 }
